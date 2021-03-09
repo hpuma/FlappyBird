d4f3b98e06aff4070175826783c72d294d5f1761
hpuma <pumahenry@live.com>
2021-03-09 01:57:33 -0500

    Finished session1

---
+++ Assets/Script/FlappyBirdController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FlappyBirdController: MonoBehaviour {
+  public float flapPower;
+  Rigidbody2D rb; //The rigidbody2D attached to flappybird
+  // Use this for initialization
+  void Start() {
+    rb = GetComponent < Rigidbody2D > (); //Getting reference to
+    flappybird 's RigidBody2D
+  }
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Space)) //If spacebar is pressed down
+    // Calls once when pressed down
+    {
+      rb.velocity = new Vector2(rb.velocity.x, flapPower); //Adds an
+      upward velocity to the Rigidbody
+    }
+  }
+}
