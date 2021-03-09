21a587692e7a85cd6aad804c10d1a5ba4397c898
hpuma <pumahenry@live.com>
2021-03-09 01:59:37 -0500

    fixed akward comments

--- Assets/Script/FlappyBirdController.cs
+++ Assets/Script/FlappyBirdController.cs
@@ -6,16 +6,15 @@ public class FlappyBirdController: MonoBehaviour {
   Rigidbody2D rb; //The rigidbody2D attached to flappybird
   // Use this for initialization
   void Start() {
-    rb = GetComponent < Rigidbody2D > (); //Getting reference to
-    flappybird 's RigidBody2D
-  }
+    rb = GetComponent < Rigidbody2D > (); //Getting reference to  flappybird 's RigidBody2D
+    }
   // Update is called once per frame
   void Update() {
     if (Input.GetKeyDown(KeyCode.Space)) //If spacebar is pressed down
     // Calls once when pressed down
     {
-      rb.velocity = new Vector2(rb.velocity.x, flapPower); //Adds an
-      upward velocity to the Rigidbody
-    }
+      rb.velocity = new Vector2(rb.velocity.x, flapPower); //Adds an upward velocity to the Rigidbody
+
+        }
   }
 }
